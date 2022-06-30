dfacf722f8d62ac5715cf85f9495a0695a415823
Alex Mur <r2d2k@mail.ru>
2022-06-30 09:24:23 +0500

    Terraform 1 task, add load balancer

---
+++ terraform/lb.tf
@@ -0,0 +1,32 @@
+resource "yandex_lb_network_load_balancer" "lb" {
+  name = "reddit-app-loadbalancer"
+
+  listener {
+    name        = "reddit-app-listener"
+    port        = 80
+    target_port = 9292
+    protocol    = "tcp"
+    external_address_spec {
+      ip_version = "ipv4"
+    }
+  }
+
+  attached_target_group {
+    target_group_id = yandex_lb_target_group.lb_tg.id
+    healthcheck {
+      name = "http"
+      http_options {
+        port = 9292
+      }
+    }
+  }
+}
+
+resource "yandex_lb_target_group" "lb_tg" {
+  name = "reddit-app-targetgroup"
+
+  target {
+    address   = yandex_compute_instance.app.network_interface.0.ip_address
+    subnet_id = var.subnet_id
+  }
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,8 @@
 output "external_ip_address_app" {
   value = yandex_compute_instance.app.network_interface.0.nat_ip_address
 }
+
+output "external_ip_address_lb" {
+  #  value = [for s in yandex_lb_network_load_balancer.lb.listener : s if s != "external_address_spec"]
+  value = yandex_lb_network_load_balancer.lb.listener
+}
